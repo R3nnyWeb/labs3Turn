09f829470393dd4384c51fe8678ad15277b3f737
Nikita_Vashkulatov <nikitavaskulatov@gmail.com>
2022-02-10 15:41:17 +0300

    Исправил код

--- Java/Lab1/src/MainClass.java
+++ Java/Lab1/src/MainClass.java
@@ -1,6 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
-        for (int i = 0; i <100 ; i++) {
+        for (int i = 0; i <1000 ; i++) {
             System.out.println("Светик, люблю вас!!");
         }
     }
