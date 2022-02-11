89976fb97405d7a7beaf3754816945bd51e834b5
Nikita_Vashkulatov <nikitavaskulatov@gmail.com>
2022-02-11 10:29:32 +0300

    Laba 1 v1.0

---
+++ Java/lab1/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        //Задание 1
+        // Вывести заданное количество случайных чисел с переходом и без перехода на новую строку.
+        System.out.println("Введите n: ");
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        int n = sc.nextInt();
+        System.out.println("Без перехода: ");
+        for (int i = 0; i <n ; i++) {
+            System.out.printf(" " + String.valueOf(rd.nextInt(100)));
+        }
+        System.out.println();
+        System.out.println("С переходом: ");
+        for (int i = 0; i <n ; i++) {
+            System.out.println(" " + String.valueOf(rd.nextInt(100)));
+        }
+    }
+}
---
+++ Java/lab1/src/App2.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class App2 {
+    public static void main(String[] args) {
+        //Задание 2
+        //Ввести с консоли n целых чисел. На консоль вывести:
+        //Элементы, которые равны полусумме соседних элементов.
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Введите n: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i =0; i<arr.length; i++) {
+            System.out.println(" Введите a[" + i +"] : ");
+         arr[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+        for (int i = 1; i <arr.length-1 ; i++) {
+            if ((arr[i-1] + arr[i+1])/2 == arr[i]){
+                System.out.println("Выполняются условия: arr["+i+"] " +arr[i]);
+            }
+        }
+    }
+}
