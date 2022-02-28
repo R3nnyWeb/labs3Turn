39ff60fd12b20d44250839f927775b7e89ce0077
R3nnyWeb <nikitavaskulatov@gmail.com>
2022-02-28 21:28:47 +0300

    Сделал метод для переделывания смежной матрицы в прямое соответсвие. Надо переделать Map

--- Discretnaya/lab1/src/main/java/Graph.java
+++ Discretnaya/lab1/src/main/java/Graph.java
@@ -1,18 +1,15 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
     private final Set<TopGraph> graphTops; //Множество вершин графа
-    private Map<TopGraph, LinkedHashSet<TopGraph>> directMatch; //Прямое соответсвие
+    private Map<TopGraph, ArrayList<TopGraph>> directMatch; //Прямое соответсвие
     private byte[][] adjacencyMatrix;  //Матрица смежности
 
      Graph(byte[][] adjacencyMatrix,int numberOfTops ){
          graphTops = new LinkedHashSet<>();
          initGraphTops(numberOfTops);
          this.adjacencyMatrix = adjacencyMatrix;
-
+         directMatch = directMatchFromAdjacencyMatrix();
      }
 
      private void initGraphTops(int numberOfTops){
@@ -24,6 +21,27 @@ public class Graph {
     public int getNumberOfTops() {
         return graphTops.size();
     }
+
+    private Map<TopGraph, ArrayList<TopGraph>> directMatchFromAdjacencyMatrix(){
+         directMatch = new LinkedHashMap<>();
+
+        for (int i = 0; i <getNumberOfTops() ; i++) {
+            ArrayList<TopGraph> directMatchTops = new ArrayList();
+            for (int j = 0; j <getNumberOfTops() ; j++) {
+                if (adjacencyMatrix[i][j] == 1){
+
+                    directMatchTops.add(new TopGraph(j));
+                }
+            }
+            directMatch.put(new TopGraph(i), directMatchTops);
+        }
+         return directMatch;
+    };
+
+
+     public Map<TopGraph, ArrayList<TopGraph>> getDirectMatch(){
+       return directMatch;
+     };
 
     @Override
     public String toString() {
--- Discretnaya/lab1/src/test/java/GraphTest.java
+++ Discretnaya/lab1/src/test/java/GraphTest.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedHashSet;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +9,7 @@ class GraphTest {
 
 
     @Test
-    void getNumberOfTops() {
+    void getNumberOfTopsTest() {
         int exceptedNumberOfTops = 7;
         byte[][] arr = new byte[exceptedNumberOfTops][exceptedNumberOfTops];
         Graph graph = new Graph(arr,exceptedNumberOfTops);
@@ -18,16 +17,74 @@ class GraphTest {
     }
 
     @Test
-    void getDirectMatch(){
+    void getDirectMatchTest(){
         int exceptedNumberOfTops = 7;
-        Map<TopGraph, LinkedHashSet<TopGraph>> exceptedDirectMatch;
+        Map<TopGraph, ArrayList<TopGraph>> exceptedDirectMatch;
+        exceptedDirectMatch = new LinkedHashMap<>(){
+            {
+                put(new TopGraph(0), new ArrayList<>(){
+                    {
+                      add(new TopGraph(0));
+                      add(new TopGraph(4));
+                      add(new TopGraph(6));
+                    }
+                });
+                put(new TopGraph(1), new ArrayList<>(){
+                    {
+                        add(new TopGraph(0));
+                        add(new TopGraph(2));
+                        add(new TopGraph(4));
+                    }
+                });
+                put(new TopGraph(2), new ArrayList<>(){
+                    {
+                        add(new TopGraph(3));
+                    }
+                });
+                put(new TopGraph(3), new ArrayList<>(){
+                    {
+                        add(new TopGraph(2));
+                        add(new TopGraph(5));
+                    }
+                });
+                put(new TopGraph(4), new ArrayList<>(){
+                    {
+                        add(new TopGraph(0));
+                        add(new TopGraph(5));
+                    }
+                });
+                put(new TopGraph(5), new ArrayList<>(){
+                    {
+                        add(new TopGraph(2));
+                        add(new TopGraph(3));
+                        add(new TopGraph(6));
+                    }
+                });
+                put(new TopGraph(6), new ArrayList<>(){
+                    {
+                        add(new TopGraph(6));
+                    }
+                });
+            }
+        };
+
         byte[][] adjacencyMatrix = {{1,0,0,0,1,0,1},
                                     {1,0,1,0,1,0,0},
                                     {0,0,0,1,0,0,0},
                                     {0,0,1,0,0,1,0},
                                     {1,0,0,0,0,1,0},
                                     {0,0,1,1,0,0,1},
-                                    {0,0,0,0,0,0,0}};
+                                    {0,0,0,0,0,0,1}};
+        Graph graph = new Graph(adjacencyMatrix,exceptedNumberOfTops);
+        Map<TopGraph, ArrayList<TopGraph>> actualDirectMatch = graph.getDirectMatch();
+        boolean result = true;
+        Set<TopGraph> exceptedGraphSet = exceptedDirectMatch.keySet();
+        Set<TopGraph> actualGraphSet = actualDirectMatch.keySet();
+            
+
+        assert(result);
+
+
 
     }
 }
