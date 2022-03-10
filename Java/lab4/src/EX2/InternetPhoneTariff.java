f22c2c03a702e39c42194f7b4428680dca790d6c
R3nnyWeb <nikitavaskulatov@gmail.com>
2022-03-10 18:21:30 +0300

    Почти доделал lab3

--- Java/lab4/src/EX2/InternetPhoneTariff.java
+++ Java/lab4/src/EX2/InternetPhoneTariff.java
@@ -1,6 +1,29 @@
 package EX2;
 
-public class InternetPhoneTariff extends  PhoneTariff{
-    private final double gigabytesOfInternet = 25;
+public class InternetPhoneTariff extends  PhoneTariff {
+    private double mBOfInternet;
 
+    public InternetPhoneTariff(int users, String name, double costInRubles, int minutesToTalk, double mBOfInternet) {
+        super(users, name, costInRubles, minutesToTalk);
+        this.mBOfInternet = mBOfInternet;
+    }
+
+    public double getmBOfInternet() {
+        return mBOfInternet;
+    }
+
+    public void setmBOfInternet(double mBOfInternet) {
+        this.mBOfInternet = mBOfInternet;
+    }
+
+    @Override
+    public String toString() {
+        return "InternetPhoneTariff{" +
+                "users=  " + getUsers() + '\'' +
+                "minutesToTalk= " + getMinutesToTalk() + '\'' +
+                "mBOfInternet=" + mBOfInternet +
+                ", name='" + name + '\'' +
+                ", costInRubles=" + costInRubles +
+                '}';
+    }
 }
--- Java/lab4/src/EX2/MobileTariff.java
+++ Java/lab4/src/EX2/MobileTariff.java
@@ -1,17 +1,42 @@
 package EX2;
 
-import java.util.Date;
 
 public abstract class MobileTariff {
     private int users;
-    private final Date minutesToTalk;
+    String name;
+    double costInRubles;
 
-    protected MobileTariff() {
+
+    public MobileTariff(int users, String name, double costInRubles) {
+        this.users = users;
+        this.name = name;
+        this.costInRubles = costInRubles;
     }
 
     public int getUsers() {
         return users;
     }
+
+    public void setUsers(int users) {
+        this.users = users;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCostInRubles() {
+        return costInRubles;
+    }
+
+    public void setCostInRubles(double costInRubles) {
+        this.costInRubles = costInRubles;
+    }
+
     public  void addUsers(int count){
         users =+ users;
     };
