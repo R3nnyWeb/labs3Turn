65e87a1018225cfcfe302749c7c14612d269d5ef
R3nny <nvaskulatov@gmail.com>
2022-04-28 15:13:20 +0300

    Java lab 7 ex 2

---
+++ Java/lab_7/src/Ex2/App.java
@@ -0,0 +1,24 @@
+package Ex2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите текст");
+        String text = scanner.nextLine();
+        System.out.println("Введите фильтрующее слово");
+        String filterWord = scanner.nextLine();
+        scanner.close();
+        text = text.toLowerCase(Locale.ROOT)
+                .replace("ё", "е")
+                .replace("й","и");
+        String[] splitedText = text.split(" ");
+        List<String> words = Arrays.stream(splitedText).toList();
+        words = words.stream().filter(s -> s.equals(filterWord)).toList();
+        System.out.println(words.size());
+
+    }
+
+}
