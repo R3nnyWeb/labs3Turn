fc3395185aeb811e13091a965b4415febb17bd79
Slava <Anohin.VA@yandex.ru>
2022-04-28 21:58:07 +0300

    java

--- Java/lab_7/src/Ex3/App.java
+++ Java/lab_7/src/Ex3/App.java
@@ -1,11 +1,14 @@
 package Ex3;
 
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
         Parser parser = new Parser();
         String text = """
                 Hello world! Second sentence.
                 New paragraph...""";
+        ArrayList<String> words;
         Text parsedText = parser.parseText(text);
         System.out.println(parsedText);
     }
