f52249d64bfa002353a7dc2dc0de4f11dd56cbb9
R3nnyWeb <78970794+R3nnyWeb@users.noreply.github.com>
2022-04-29 11:19:30 +0300

    Исправил java 7_2

--- Java/lab_7/src/Ex2/App.java
+++ Java/lab_7/src/Ex2/App.java
@@ -2,7 +2,16 @@ package Ex2;
 
 import java.util.*;
 
+
 public class App {
+
+    public static String modifyString(String s){
+       s = s.toLowerCase(Locale.ROOT)
+                .replace("ё", "е")
+                .replace("й","и");
+
+        return s;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите текст");
@@ -10,12 +19,17 @@ public class App {
         System.out.println("Введите фильтрующее слово");
         String filterWord = scanner.nextLine();
         scanner.close();
-        text = text.toLowerCase(Locale.ROOT)
-                .replace("ё", "е")
-                .replace("й","и");
+        text = modifyString(text);
+        text =  text.replace(",","")
+                .replace("?", "")
+                .replace("!", "")
+                .replace(".", "")
+                .replace(";", "");
+        filterWord = modifyString(filterWord);
         String[] splitedText = text.split(" ");
         List<String> words = Arrays.stream(splitedText).toList();
-        words = words.stream().filter(s -> s.equals(filterWord)).toList();
+        String finalFilterWord = filterWord;
+        words = words.stream().filter(s -> s.equals(finalFilterWord)).toList();
         System.out.println(words.size());
     }
 }
