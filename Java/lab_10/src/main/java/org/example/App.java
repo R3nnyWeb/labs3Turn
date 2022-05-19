c2bfa5e49cfdfee3ff95e292a1fb943c5ebf8cd1
Slava <Anohin.VA@yandex.ru>
2022-05-19 20:26:05 +0300

    tmp

--- Java/lab_10/src/main/java/org/example/App.java
+++ Java/lab_10/src/main/java/org/example/App.java
@@ -1,14 +1,5 @@
 package org.example;
 
-
-import org.example.models.City;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App
